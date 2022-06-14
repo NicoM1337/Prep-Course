473b7493456bfe01563453dfe000583c0e5ad92b
Nicolas Chamorro <107502580+NicoM1337@users.noreply.github.com>
2022-06-14 16:59:29 -0300

    02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = _nico_;
 const nuevoNum = 9;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
