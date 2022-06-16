50f003a3245ed057091b98ab704b31fe7063f8a7
Nicolas Chamorro <107502580+NicoM1337@users.noreply.github.com>
2022-06-16 17:43:01 -0300

    First commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,13 +1,13 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = nico;
+const nuevaString = 'nico';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 9;
 
 // Crea una variable booleana:
-const nuevoBool = False;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
@@ -28,32 +28,32 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código: 
-  
+  return str;
 }
 
-function suma(x, y) {
+function sumar(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  return x + y;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  return x - y;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  return x * y;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return x / y;
 }
 
 function sonIguales(x, y) {
