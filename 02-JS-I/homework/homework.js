0fa9f78048a7df1fe785d92a4665c6c7ce0cc6b6
Nicolas Chamorro <107502580+NicoM1337@users.noreply.github.com>
2022-06-16 18:15:14 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,14 +60,14 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  true
+  false
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  true
+   
 }
 
 function menosQueNoventa(num) {
